4a260f1501b2ccb9b0654be962f37f7322ea86e7
Shalini Yadav <shaliniyadav.ait@gmail.com>
2021-02-09 10:04:14 +0800

    update on the linked list node where cycle begins

--- leetcode/linkedlist/cycleII.go
+++ leetcode/linkedlist/cycleII.go
@@ -3,47 +3,35 @@ package linkedlist
 // https://leetcode.com/problems/linked-list-cycle-ii/
 func detectCycle(head *ListNode) *ListNode {
 
-	if head != nil {
-		return nil
-	}
 	
-	cycle := false
-	single := head
-	double := head.Next
+    if head == nil || head.Next == nil{
+        return nil
+    }
 
-	for double != nil && double.Next != nil {
-		single = single.Next
-		double = double.Next.Next
-		if single == double {
-			cycle = true
-			break
-		}
-
-	}
-
-	if cycle == false {
-		return nil
-	}
-
-	perimeter := 1
-	double = double.Next
-	for single != double {
-		double = double.Next
-		perimeter++
-	}
-
-	single = head
-	double = head
-	count := 1
-	for count <= perimeter {
-		double = double.Next
-		count++
-	}
-
-	for single != double {
-		single = single.Next
-		double = double.Next
-	}
-
-	return single
+    single := head.Next
+    double := head.Next.Next
+    cycle := false
+    
+    for double != nil && double.Next != nil && double.Next.Next != nil{
+        single = single.Next
+        double = double.Next.Next
+        
+        if single == double{
+          cycle = true
+          break
+        }
+    }
+    
+    if cycle == true{
+    
+    single = head
+    for single != double{
+        single = single.Next
+        double = double.Next
+    }
+    
+    return single
+    }
+    
+    return nil
 }
